2c2e65730848001f322b9fb1a9f65cc2bde35d31
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-27 21:22:13 -0400

    Entorno, django, app, inicio, models, listos

--- Plantitarica/Plantitarica/urls.py
+++ Plantitarica/Plantitarica/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('PlantitaricaApp/', include('PlantitaricaApp.urls')),
+    path('', include('PlantitaricaApp.urls')),
+    
 ]
--- Plantitarica/PlantitaricaApp/views.py
+++ Plantitarica/PlantitaricaApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+	context={}
+	return render(request,'PlantitaricaApp/inicio.html', context)
