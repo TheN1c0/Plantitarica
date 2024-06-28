ec0e91ea1528da1f088dfb9b27914e8882a83276
BMolinae <ben.molina@duocuc.cl>
2024-06-27 23:09:57 -0400

    Errores y mas errores

--- Plantitarica/PlantitaricaApp/urls.py
+++ Plantitarica/PlantitaricaApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('agregar-producto/', views.agregar_producto, name='agregar_producto'),
     path('btnAgregarProduc', views.btnAgregarProduc, name='btnAgregarProduc'),
     path('Productos/', views.Productos, name='Productos'),
+    path('Maceteros/', views.PMaceteros, name='Maceteros'),
+    path('Insumos/', views.PInsumos, name='Insumos'),
 
 ]
--- Plantitarica/PlantitaricaApp/views.py
+++ Plantitarica/PlantitaricaApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ProductoForm
-
+from .models import Plantas, Maceteros, Insumos, Clientes
 # Create your views here.
 def inicio(request):
 	context={}
@@ -11,8 +11,19 @@ def agregar_producto(request):
 	return render(request,'PlantitaricaApp/agregar_producto.html', context)
 
 def Productos(request):
-	context={}
+	Planta = Plantas.object.all()
+	context={"Plantas":Planta}
 	return render(request,'PlantitaricaApp/Productos.html', context)
+
+def PMaceteros(request):
+	Macetero = Maceteros.object.all()
+	context={"Maceteros":Macetero}
+	return render(request,'PlantitaricaApp/Maceteros.html', context)
+
+def PInsumos(request):
+	Insumo = Insumos.object.all()
+	context={"Insumos":Insumo}
+	return render(request,'PlantitaricaApp/Insumos.html', context)
 
 
 def btnAgregarProduc(request):
@@ -24,3 +35,4 @@ def btnAgregarProduc(request):
         else:
             form = ProductoForm()
         return render(request, 'plantitaricaApp/agregar_producto.html', {'form': form})
+
