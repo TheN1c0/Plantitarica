3a29e19b9d33477e56952b06e1ddb837c7db08b1
infrunnami <correoepicgames9@gmail.com>
2024-06-28 15:15:12 -0400

    arreglo de navbar y css

--- Plantitarica/PlantitaricaApp/models.py
+++ Plantitarica/PlantitaricaApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Plantas(models.Model):
     id_planta  = models.AutoField(db_column='idPlanta', primary_key=True) 
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=100)
     precio     = models.IntegerField(blank=False, null=False)
     foto = models.ImageField(upload_to='Plantas/',null=True, blank=True,verbose_name='Foto de la planta')
 
@@ -14,7 +14,7 @@ class Plantas(models.Model):
     
 class Maceteros(models.Model):
     id_Macetero = models.AutoField(db_column='idMacetero', primary_key=True) 
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=100)
     precio     = models.IntegerField(blank=False, null=False)
     descripcion = models.CharField(max_length=500, blank=False, null=False)
     foto = models.ImageField(upload_to='Maceteros/',null=True, blank=True,verbose_name='Foto del Macetero')
@@ -27,7 +27,7 @@ class Maceteros(models.Model):
     
 class Insumos(models.Model):
     id_Insumo  = models.AutoField(db_column='idInsumo', primary_key=True) 
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=100)
     precio     = models.IntegerField(blank=False, null=False)
     descripcion = models.CharField(max_length=500, blank=False, null=False)
     foto = models.ImageField(upload_to='Insumos/',null=True, blank=True,verbose_name='Foto del Insumo')
@@ -41,7 +41,7 @@ class Insumos(models.Model):
 
 class Clientes(models.Model):
     id_Cliente  = models.AutoField(db_column='id_Cliente', primary_key=True) 
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=100)
     email = models.EmailField()
     contrasena = models.CharField(max_length=30)
     numero = models.IntegerField(blank=True, null=True)
