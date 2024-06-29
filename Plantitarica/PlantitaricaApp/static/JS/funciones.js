9eeb52566443c1ae8b4a919ec967b3938dfbbeca
infrunnami <correoepicgames9@gmail.com>
2024-06-28 20:08:17 -0400

    sigan viendo

---
+++ Plantitarica/PlantitaricaApp/static/JS/funciones.js
@@ -0,0 +1,10 @@
+    //agregar la clase 'active' al elemento clickeado y quitarla de los demás
+    var elementosNavbar = document.querySelectorAll('.mini-nav ul li');
+    elementosNavbar.forEach(function(elemento) {
+        elemento.addEventListener('click', function() {
+            elementosNavbar.forEach(function(el) {
+                el.classList.remove('active');
+            });
+            elemento.classList.add('active');
+        });
+    });
