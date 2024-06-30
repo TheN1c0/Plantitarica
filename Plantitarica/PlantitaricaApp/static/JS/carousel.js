36ae959058b8649df19ee24602e2a68f3cb437be
infrunnami <correoepicgames9@gmail.com>
2024-06-29 23:53:08 -0400

    inicio actualizado

--- Plantitarica/PlantitaricaApp/static/JS/carousel.js
+++ Plantitarica/PlantitaricaApp/static/JS/carousel.js
@@ -1,21 +1,18 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Cambia de imagen cada 2 segundos
+}
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
 }
